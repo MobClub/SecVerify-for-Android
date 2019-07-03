1234453fc809ea2f6d2043bc132f8f29aecd01c7
Jackie Wayne <jackiewayne2046@163.com>
2019-07-03 10:07:55 +0800

    upgrade to 1.0.1

--- SecVerify-Demo/src/com/mob/secverify/demo/MainActivity.java
+++ SecVerify-Demo/src/com/mob/secverify/demo/MainActivity.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mob.secverify.CustomUIRegister;
+import com.mob.secverify.CustomViewClickListener;
 import com.mob.secverify.SecVerify;
 import com.mob.secverify.OperationCallback;
 import com.mob.secverify.VerifyCallback;
@@ -25,9 +31,11 @@ import com.mob.secverify.demo.ui.component.CommonProgressDialog;
 import com.mob.secverify.demo.util.Const;
 import com.mob.secverify.exception.VerifyErr;
 import com.mob.secverify.exception.VerifyException;
+import com.mob.secverify.log.VerifyLog;
 import com.mob.secverify.ui.component.VerifyCommonButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import pl.droidsonroids.gif.GifImageView;
 
@@ -84,6 +92,13 @@ public class MainActivity extends BaseActivity {
 	}
 
 	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		// Demo为了演示效果，动态授权结束后做一次预登录
+		preVerify();
+	}
+
+	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 	}
@@ -136,11 +151,34 @@ public class MainActivity extends BaseActivity {
 			@Override
 			public void onComplete(Object data) {
 				// Nothing to do
+				if (devMode) {
+					Toast.makeText(MainActivity.this, "预登录成功", Toast.LENGTH_SHORT).show();
+				}
 			}
 
 			@Override
 			public void onFailure(VerifyException e) {
 				// Nothing to do
+				if (devMode) {
+					// 登录失败
+					Log.e(TAG, "preVerify failed", e);
+					// 错误码
+					int errCode = e.getCode();
+					// 错误信息
+					String errMsg = e.getMessage();
+					// 更详细的网络错误信息可以通过t查看，请注意：t有可能为null
+					Throwable t = e.getCause();
+					String errDetail = null;
+					if (t != null) {
+						errDetail = t.getMessage();
+					}
+
+					String msg = "错误码: " + errCode + "\n错误信息: " + errMsg;
+					if (!TextUtils.isEmpty(errDetail)) {
+						msg += "\n详细信息: " + errDetail;
+					}
+					Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
 	}
@@ -149,7 +187,10 @@ public class MainActivity extends BaseActivity {
 	 * 免密登录
 	 */
 	private void verify() {
+		// 自定义UI
 		customizeUi();
+		// 添加自定义控件
+		// addCustomView();
 
 		CommonProgressDialog.showProgressDialog(this);
 		SecVerify.verify(new VerifyCallback() {
@@ -158,6 +199,13 @@ public class MainActivity extends BaseActivity {
 				// 用户点击“其他登录方式”，处理自己的逻辑
 				CommonProgressDialog.dismissProgressDialog();
 				Toast.makeText(MainActivity.this, "其他账号登录", Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onUserCanceled() {
+				// 用户点击“关闭按钮”或“物理返回键”取消登录，处理自己的逻辑
+				CommonProgressDialog.dismissProgressDialog();
+				Toast.makeText(MainActivity.this, "用户取消登录", Toast.LENGTH_SHORT).show();
 			}
 
 			@Override
@@ -201,6 +249,9 @@ public class MainActivity extends BaseActivity {
 								msg = "当前网络异常";
 							}
 							Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+							// Demo为了演示效果，登录失败时也做一次预登录
+							preVerify();
 						}
 					});
 				}
@@ -228,10 +279,10 @@ public class MainActivity extends BaseActivity {
 				}
 				if (!devMode) {
 					msg = "当前网络异常";
-					if (errCode == VerifyErr.C_ONE_KEY_USER_CANCEL_GRANT.getCode()
-					|| errCode == VerifyErr.C_LACK_OF_PERMISSIONS.getCode()
+					if (errCode == VerifyErr.C_LACK_OF_PERMISSIONS.getCode()
 					|| errCode == VerifyErr.C_NO_SIM.getCode()
-					|| errCode == VerifyErr.C_UNSUPPORTED_OPERATOR.getCode()) {
+					|| errCode == VerifyErr.C_UNSUPPORTED_OPERATOR.getCode()
+					|| errCode == VerifyErr.C_CELLULAR_DISABLED.getCode()) {
 						msg = errMsg;
 					}
 				}
@@ -281,20 +332,79 @@ public class MainActivity extends BaseActivity {
 				.setCheckboxDefaultState(true)
 				// 隐私协议字体颜色资源ID（自定义隐私协议的字体颜色也受该值影响）
 				.setAgreementColorId(R.color.sec_verify_demo_main_color)
-//				// 自定义隐私协议一文字资源ID（移动&电信）
+//				// 自定义隐私协议一文字资源ID
 //				.setCusAgreementNameId1(R.string.sec_verify_demo_customize_agreement_name_1)
-//				// 自定义隐私协议一控件ID（联通）
-//				.setCuccAgreementComponentId1(R.id.customize_agreement_1)
 //				// 自定义隐私协议一URL
 //				.setCusAgreementUrl1("http://www.baidu.com")
-//				// 自定义隐私协议二文字资源ID（移动&电信）
+//				// 自定义隐私协议二文字资源ID
 //				.setCusAgreementNameId2(R.string.sec_verify_demo_customize_agreement_name_2)
-//				// 自定义隐私协议二控件ID（联通）
-//				.setCuccAgreementComponentId2(R.id.customize_agreement_2)
 //				// 自定义隐私协议二URL
 //				.setCusAgreementUrl2("https://www.jianshu.com")
 				.build();
 		SecVerify.setUiSettings(uiSettings);
+	}
+
+	/**
+	 * 添加自定义view
+	 */
+	private void addCustomView() {
+		// view仅用于使btn1和btn2水平居中显示
+		View view = new View(this);
+		view.setId(R.id.customized_view_id);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(1,1);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		params.bottomMargin = 250;
+		view.setLayoutParams(params);
+
+		// 自定义按钮1
+		Button btn1 = new Button(this);
+		btn1.setId(R.id.customized_btn_id_1);
+		btn1.setText("按钮1");
+		btn1.setTextColor(Color.BLACK);
+		btn1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+		RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+		params1.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		params1.addRule(RelativeLayout.LEFT_OF, view.getId());
+		params1.bottomMargin = 250;
+		params1.rightMargin = 10;
+		btn1.setLayoutParams(params1);
+
+		// 自定义按钮2
+		Button btn2 = new Button(this);
+		btn2.setId(R.id.customized_btn_id_2);
+		btn2.setText("按钮2");
+		btn2.setTextColor(Color.BLACK);
+		btn2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+		RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+		params2.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		params2.addRule(RelativeLayout.RIGHT_OF, view.getId());
+		params2.bottomMargin = 250;
+		params2.leftMargin = 10;
+		btn2.setLayoutParams(params2);
+
+		List<View> views = new ArrayList<View>();
+		views.add(view);
+		views.add(btn1);
+		views.add(btn2);
+
+		CustomUIRegister.addCustomizedUi(views, new CustomViewClickListener() {
+			@Override
+			public void onClick(View view) {
+				int id = view.getId();
+				String msg = "";
+				if (id == R.id.customized_btn_id_1) {
+					msg = "按钮1 clicked";
+					// 自定义控件点击时，SecVerify默认不关闭授权页面，若需关闭，可调用该方法
+					SecVerify.finishOAuthPage();
+				} else if (id == R.id.customized_btn_id_2) {
+					msg = "按钮2 clicked";
+				}
+				// 关闭加载框
+				CommonProgressDialog.dismissProgressDialog();
+				Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	private void gotoSuccessActivity(LoginResult data) {
